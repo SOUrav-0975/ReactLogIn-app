58fe80f3bd55ad453dd81a4c8a3dc66550fed364
SOUrav-0975 <souravk0975@gmail.com>
2022-11-18 01:02:51 +0530

    useTreducer task

--- 01-starting-project/src/App.js
+++ 01-starting-project/src/App.js
@@ -13,7 +13,7 @@ if(storeData === '1'){
   setIsLoggedIn(true)
 }
   },[])
-  const loginHandler = (email, password,college) => {
+  const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
     localStorage.setItem('isLoggedIn','1')
